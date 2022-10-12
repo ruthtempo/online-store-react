780c46b14b81b2f111a69a2fa7718e8e2616c268
Ruth Temporal <ruthtemp@gmail.com>
2022-10-12 11:22:54 +0200

    grid cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -7,20 +7,29 @@ export const Cart = () => {
 
   return (
     <Row>
-      {cart.map((cartElem) => (
-        <Col>
-          <ProductCard product={cartElem.product} />
-          <Button
-            className="mt-3"
-            onClick={() => removeFromCart(cartElem.product)}
-          >
-            Remove from Cart
-          </Button>
-        </Col>
-      ))}
-
-      <Col>data form</Col>
-      <Col>my basket</Col>
+      <Col md={3}>
+        {cart.map((cartElem) => (
+          <div>
+            <ProductCard product={cartElem.product} />
+            <Button
+              className="mt-3"
+              onClick={() => removeFromCart(cartElem.product)}
+            >
+              Remove from Cart
+            </Button>
+          </div>
+        ))}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <Card.Body>data form</Card.Body>
+        </Card>
+      </Col>
+      <Col md={4}>
+        <Card>
+          <Card.Body>My Basket</Card.Body>
+        </Card>
+      </Col>
     </Row>
   );
 };
